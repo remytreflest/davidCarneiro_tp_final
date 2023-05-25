70eeda1b5167fd4ebd50da29b69280cac4c3777f
treflest remy <mrtreflestremy@outlook.com>
2023-05-25 12:27:13 +0200

    premier fonctionnalité ok + barre navigation

--- src/router/Routeur.js
+++ src/router/Routeur.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Error from './Error'
 import Layout from '../layouts/layout'
+import CrewDetailsCard from '../components/CrewDetailsCard'
 
 const Routeur = ({ text }) => {
     return (
@@ -10,7 +11,9 @@ const Routeur = ({ text }) => {
                 <Layout>
                     <Routes>
                         <Route exact path='/' element={<Home />} />
+                        <Route exact path='/crew/:id' element={<CrewDetailsCard />} />
                         <Route path='*' element={<Error text='404' />} />
+                        <Route path='/404' element={<Error text='404' />} />
                     </Routes>
                 </Layout>
             </div>
